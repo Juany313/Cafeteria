281263ef96f0761526b6f3c7e52f64b8c8bc3164
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-11-21 07:25:02 -0300

    header listo

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
-const { src, dest, watch, series, parallel } = require('gulp');
-const gulpSass = require('gulp-dart-sass'); 
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer')
 
+import { src, dest, watch, series, parallel } from 'gulp';
 
+//Css y Sass
+import gulpSass from 'gulp-dart-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+
+//Imagenes
+import imagemin from 'gulp-imagemin';
+import image from 'gulp-image';
+import webp from 'gulp-webp';
+import avif from 'gulp-avif';
 
 // Función para compilar Sass
 function css(done) {
@@ -17,27 +24,51 @@ function css(done) {
         .pipe(postcss([autoprefixer()]).on('error', (err) => {
             console.error('Error en PostCSS:', err.message); // Mostrar solo el mensaje del error
         }))
-        .pipe(dest('dist/css')); // Paso 3: guardar el css
+        .pipe(dest('build/css')); // Paso 3: guardar el css
 
     done();
+}
+function imagenes() {
+    return src('src/img/**/*', { allowEmpty: true }) // Permitir carpetas vacías
+        .pipe(dest('build/img')); // Copiar sin ningún procesamiento
+}
+
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp({
+            quality: 50,
+        }))
+        .pipe(dest('build/img'))
+}
+function versionAvif() {
+    return src('src/img/**/*.{png,jpg}')  
+        .pipe(avif({
+            quality: 50,
+        }))
+        .pipe(dest('build/img'))
+        .on('end', () => {
+            console.log('Conversión a AVIF completada');
+        });
 }
 
 // Función para el modo de desarrollo (watch)
 function dev() {
     // Observar cambios en el archivo Sass y compilar automáticamente
-    watch('src/scss/**/*.scss', css)
+    watch('src/scss/**/*.scss', css);
+    // Observar cambios en la carpeta img por si agrego o quito imagenes
+    //watch('src/img/**/*', imagenes);
 }
 
-function tareaDefault() {
-    console.log('soy la tarea default');
-    
-}
 
-// Exportar tareas
-exports.css = css;
-exports.dev = dev;
-exports.default = series(css, dev);
+export { 
+    css, 
+    dev, 
+    imagenes,
+    versionWebp,
+    versionAvif
+};
 
+export default series( css, dev);
 //series - Se inicia una tarea y cuando finaliza inicia la siguiente.
 
 //parallel - Todas inician al mismo tiempo.
