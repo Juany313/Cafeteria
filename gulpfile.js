cf3b7594fdf16c1b28b9dfb0aed5665c5eb57e2a
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-11-29 07:08:28 -0300

    pages - cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,13 @@
 
-import { src, dest, watch, series, parallel } from 'gulp';
+import { src, dest, watch, series } from 'gulp';
 
 //Css y Sass
 import gulpSass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
-import sourcemaps from 'gulp-sourcemaps'
+import sourcemaps from 'gulp-sourcemaps';
+import cssnano from 'cssnano';
 
-//Imagenes
-import imagemin from 'gulp-imagemin';
-import image from 'gulp-image';
-import webp from 'gulp-webp';
-import avif from 'gulp-avif';
 
 // Función para compilar Sass
 function css(done) {
@@ -23,7 +19,7 @@ function css(done) {
             console.error('Error en Sass:', err.message); // Muestra solo el mensaje
             this.emit('end'); // Continúa la ejecución
         })
-        .pipe(postcss([autoprefixer()]).on('error', (err) => {
+        .pipe(postcss([autoprefixer(), cssnano()]).on('error', (err) => {
             console.error('Error en PostCSS:', err.message); // Mostrar solo el mensaje del error
         }))
         .pipe(sourcemaps.write('.'))
@@ -32,28 +28,6 @@ function css(done) {
     done();
 }
 
-function imagenes() {
-    return src('src/img/**/*', { allowEmpty: true }) // Permitir carpetas vacías
-        .pipe(dest('build/img')); // Copiar sin ningún procesamiento
-}
-
-function versionWebp() {
-    return src('src/img/**/*.{png,jpg}')
-        .pipe(webp({
-            quality: 50,
-        }))
-        .pipe(dest('build/img'))
-}
-function versionAvif() {
-    return src('src/img/**/*.{png,jpg}')  
-        .pipe(avif({
-            quality: 50,
-        }))
-        .pipe(dest('build/img'))
-        .on('end', () => {
-            console.log('Conversión a AVIF completada');
-        });
-}
 
 // Función para el modo de desarrollo (watch)
 function dev() {
@@ -64,13 +38,7 @@ function dev() {
 }
 
 
-export { 
-    css, 
-    dev, 
-    imagenes,
-    versionWebp,
-    versionAvif
-};
+
 
 export default series( css, dev);
 //series - Se inicia una tarea y cuando finaliza inicia la siguiente.
